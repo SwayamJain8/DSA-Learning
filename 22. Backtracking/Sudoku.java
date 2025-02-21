72f3fcc89e4c9f57acbe18ab30d7bbeda31d5947
swayam <jainswyam4006@gmail.com>
2025-02-21 23:49:17 +0530

    Add comment to Sudoku solver and remove temporary file

--- 22. Backtracking/Sudoku.java
+++ 22. Backtracking/Sudoku.java
@@ -69,6 +69,7 @@ public class Sudoku {
     }
 
     public static void main(String[] args) {
+        // The given question must be correct
         int sudoku[][] = {
                 { 0, 0, 8, 0, 0, 0, 0, 0, 0 },
                 { 4, 9, 0, 1, 5, 7, 0, 0, 2 },
